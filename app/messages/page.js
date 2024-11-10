c99f33377aa7cf5891fc290b856ef9f23934eea9
Kushagra Pandey <kushagra.15554@gmail.com>
2024-11-10 17:41:03 +0530

    Learned how to control caching in next

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,6 +1,10 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from 'next/cache';
+// export const revalidate = 5;
+// export const dynamic = 'force-dynamic' To avoid caching
 
 export default async function MessagesPage() {
+  unstable_noStore(); //Also done to avoid caching
   const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
