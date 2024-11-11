860dac0518b8a9337e159a3350b10f4d1ec3439b
Kushagra Pandey <kushagra.15554@gmail.com>
2024-11-11 21:12:45 +0530

    Learned how to control data caching and how to cache data when we own our database

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,12 +1,19 @@
-import Messages from '@/components/messages';
-import { unstable_noStore } from 'next/cache';
+import Messages from "@/components/messages";
+import { getMessages } from "@/lib/messages";
+// import { unstable_noStore } from 'next/cache';
 // export const revalidate = 5;
-// export const dynamic = 'force-dynamic' To avoid caching
+// export const dynamic = 'force-dynamic' //To avoid caching
 
-export default async function MessagesPage() {
-  unstable_noStore(); //Also done to avoid caching
-  const response = await fetch('http://localhost:8080/messages');
-  const messages = await response.json();
+export default function MessagesPage() {
+  // unstable_noStore(); //Also done to avoid caching
+  // const response = await fetch("http://localhost:8080/messages", {
+  //   next: {
+  //     tags: ["msg"],
+  //   },
+  // });
+  // const messages = await response.json();
+
+  const messages = getMessages();
 
   if (!messages || messages.length === 0) {
     return <p>No messages found</p>;
